1b7432a0d24d0f5553a701ab35fbc1f8df66c725
Rhy Universe <rhyunv@gmail.com>
2024-11-19 20:19:54 +0700

    Create 1.5.c

---
+++ 1.5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    float teliti = 0.1234567809876543210;
+    double lebih_teliti = 0.1234567809876543210;
+
+    printf("Nilai dengan float \t%.5f\n", teliti);
+    printf("Nilai dengan double \t%.5f\n", lebih_teliti);
+}
